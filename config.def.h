e04fb733b736a379e969ceb7901e3d2d505bdc30
Vasco Nunes <contact@vasco.dev>
2021-12-20 15:14:20 +0000

    Make mpv float

--- config.def.h
+++ config.def.h
@@ -449,7 +449,7 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
 	RULE(.class = "Gimp", .tags = 1 << 4)
-	RULE(.class = "Firefox", .tags = 1 << 7)
+	RULE(.class = "mpv", .isfloating = 1)
 	#if SCRATCHPADS_PATCH
 	RULE(.instance = "spterm", .tags = SPTAG(0), .isfloating = 1)
 	#endif // SCRATCHPADS_PATCH
